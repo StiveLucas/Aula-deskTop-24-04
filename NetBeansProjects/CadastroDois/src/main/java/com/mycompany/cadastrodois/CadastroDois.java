c0ddcb2316b9db0f4ecac8c7b59d7e291dcbff94
StiveLucas <161142347+StiveLucas@users.noreply.github.com>
2025-04-24 11:30:25 -0300

    Add files via upload

---
+++ NetBeansProjects/CadastroDois/src/main/java/com/mycompany/cadastrodois/CadastroDois.java
@@ -0,0 +1,9 @@
+package com.mycompany.cadastrodois;
+
+
+public class CadastroDois {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ NetBeansProjects/CadastroDois/src/main/java/com/mycompany/cadastrodois/modelo/Pessoa.java
@@ -0,0 +1,44 @@
+package com.mycompany.cadastrodois.modelo;
+
+public class Pessoa {
+    private String nome;
+    private int id;
+    private int idade;
+
+    public Pessoa() {
+        
+    }
+
+    public Pessoa(String nome, int id, int idade) {
+        this.nome = nome;
+        this.id = id;
+        this.idade = idade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
+    
+}
